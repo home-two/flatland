f2fc3b311541ba4d96c080631f212b0e1fa42503
Jason Kuhrt <jasonkuhrt@me.com>
2016-05-16 11:47:07 -0400

    Pass the ball to James

--- src/main.js
+++ src/main.js
@@ -2,34 +2,36 @@ import FB from "firebase"
 import uuid from "uuid"
 
 
+
 const fbUri = "https://flatlander.firebaseio.com"
 const fb = new FB(fbUri)
 const id = uuid.v4()
-// const colors = ["red", "green", "blue", "orange", "purple"]
+const colors = [ "red", "green", "blue", "orange", "purple" ]
 
-const initFirebase = () => {
+const initFirebase = () =>
   fb.set({
     hello: "world",
     users: {
       bla: "bloo"
     }
   })
-}
 
 const setupUser = () => {
   fb.child("users").update({
     [id]: {
-      x: 0,
-      y: 0,
-      color: "black"
+      x: Math.random() * 800,
+      y: Math.random() * 800,
+      color: colors[Math.floor(Math.random() * colors.length)],
     },
   })
 }
 
 fb.once("value", (snapshot) => {
-  if (snapshot.exists()) {
-    setupUser()
-  } else {
+  if (!snapshot.exists()) {
     initFirebase()
+    .then(() => console.log("Initialized Database"))
+    .then(() => setupUser())
+  } else {
+    setupUser()
   }
 })
