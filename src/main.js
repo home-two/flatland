90a6009158a6924f2aec95c60913ae30b985241c
James Paterson <j@presstube.com>
2016-05-16 11:22:43 -0400

    initializing firebase and rough user spike

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
 import FB from "firebase"
-
+import uuid from "uuid"
 
 
 const fbUri = "https://flatlander.firebaseio.com"
 const fb = new FB(fbUri)
+const id = uuid.v4()
+// const colors = ["red", "green", "blue", "orange", "purple"]
 
+const initFirebase = () => {
+  fb.set({
+    hello: "world" + Math.random(),
+    users: {
+      bla: "bloo"
+    }
+  })
+}
 
+const setupUser = () => {
+  fb.child("users").set({
+    [id]: {
+      x: 0,
+      y: 0,
+      color: "black"
+    },
+  })
+}
 
-fb.set({
-  hello: "world!",
+fb.once("value", (snapshot) => {
+  console.log('ss: ', snapshot.exists())
+  if (snapshot.exists()) {
+    setupUser()
+  } else {
+    initFirebase()
+  }
 })
+
