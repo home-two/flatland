c989dc13606cf4fcc67c6c76862a8bebb6d93bfc
James Paterson <j@presstube.com>
2016-05-30 10:20:44 -0400

    spiking new drag based on mouse events

--- src/components/user.js
+++ src/components/user.js
@@ -6,7 +6,30 @@ export default class User extends React.Component {
   }
 
   state = {
-    dragOffset: { x: 0, y: 0 },
+    dragOffset: { 
+      x: 0, 
+      y: 0,
+      mouseDown: false
+    },
+  }
+
+  // componentDidMount () {
+  //   console.log("comp did mount")
+  //   window.addEventListener("mouseUp", )
+  // }
+
+  // componentWillUnmount () {
+  //   console.log("comp will unmount")
+  // }
+
+  onMouseMove () {
+    console.log("mouse move")
+  }
+
+  onMouseUp () {
+    console.log("mouse up: ", this)
+    window.removeEventListener("mousemove", this.onMouseMove)
+    window.removeEventListener("mouseup", this.onMouseUp)
   }
 
   render () {
@@ -23,39 +46,74 @@ export default class User extends React.Component {
           top: y,
           backgroundColor: color
         }}
-        draggable={true}
-        onDragStart={(e) => {
-          console.log("DRAG_START")
+
+        onMouseDown={(e) => {
+          console.log("mouse down")
           const bounds = e.target.getBoundingClientRect()
           this.setState({
+            mouseDown: true,
             dragOffset: {
               x: e.clientX - bounds.left,
               y: e.clientY - bounds.top,
             }
+          }, () => {
+            window.addEventListener("mousemove", this.onMouseMove.bind(this))
+            window.addEventListener("mouseup", this.onMouseUp.bind(this))
           })
         }}
-        onDrag={(e) => {
-          const { dragOffset } = this.state
-          console.log("e.clientY", e.clientY)
-          console.log("e.screenY", e.clientY)
-          onMove({
-            id,
-            x: e.clientX - dragOffset.x,
-            y: e.clientY - dragOffset.y,
-          })
-        }}
-        onDragEnd={(e) => {
-          const bounds = e.target.getBoundingClientRect()
-          console.log("====DRAG_END====")
-          console.log("e.clientY", e.clientY)
-          console.log("e.screenY", e.clientY)
-          console.log("e.screenY - bounds.height", e.clientY - bounds.height)
-          onMove({
-            id,
-            x: e.clientX,
-            y: e.clientY - bounds.height,
-          })
-        }}
+
+
+
+        // onMouseMove={(e) => {
+        //   // NB: remember e.nativeEvent.movementX/Y
+        //   const {dragOffset} = this.state
+        //   if(this.state.mouseDown) {
+        //     onMove({
+        //       id,
+        //       x: e.clientX - dragOffset.x,
+        //       y: e.clientY - dragOffset.y,
+        //     })
+        //   }
+        // }}
+
+        // onMouseUp={(/*e*/) => {
+        //   console.log("mouse up")
+        //   this.setState({mouseDown:false})
+        // }}
+
+        // draggable={true}
+        // onDragStart={(e) => {
+        //   console.log("DRAG_START")
+        //   const bounds = e.target.getBoundingClientRect()
+        //   this.setState({
+        //     dragOffset: {
+        //       x: e.clientX - bounds.left,
+        //       y: e.clientY - bounds.top,
+        //     }
+        //   })
+        // }}
+        // onDrag={(e) => {
+        //   const { dragOffset } = this.state
+        //   console.log("e.clientY", e.clientY)
+        //   console.log("e.screenY", e.clientY)
+        //   onMove({
+        //     id,
+        //     x: e.clientX - dragOffset.x,
+        //     y: e.clientY - dragOffset.y,
+        //   })
+        // }}
+        // onDragEnd={(e) => {
+        //   const bounds = e.target.getBoundingClientRect()
+        //   console.log("====DRAG_END====")
+        //   console.log("e.clientY", e.clientY)
+        //   console.log("e.screenY", e.clientY)
+        //   console.log("e.screenY - bounds.height", e.clientY - bounds.height)
+        //   onMove({
+        //     id,
+        //     x: e.clientX,
+        //     y: e.clientY - bounds.height,
+        //   })
+        // }}
       ></div>
     )
   }
