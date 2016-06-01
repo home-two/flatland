e768e13a47805a1181d8063322391f1cbe289f21
Jason Kuhrt <jasonkuhrt@me.com>
2016-06-01 10:43:36 -0400

    Inserted client "Login" link.

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,9 @@ import _ from "lodash"
 
 import User from "./user"
 
+const appId = "9b5fb372b18bd26c6936"
+
+
 export default class App extends React.Component {
 
   static propTypes = {
@@ -14,6 +17,7 @@ export default class App extends React.Component {
     const {moveUser} = this.props.controller
     return (
       <div>
+        <a href={`https://github.com/login/oauth/authorize?scope=user:email&client_id=${appId}`}>Login</a>
         {_.map(users, (user, key) =>
           <User key={key}  user={{id: key, ...user}} onMove={moveUser}/>
         )}
